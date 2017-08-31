353b93d53d97715df46954a4f9a05092139f85fd
Osvaldo <osvaldo.ramirez@upr.edu>
2017-08-31 00:46:12 -0400

    Excersice 3 created

---
+++ Lab0/Excersice3/Excersice3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Excersice3
+//
+//  Created by Osvaldo A Ramirez on 8/31/17.
+//  Copyright © 2017 Osvaldo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
