2b3c76b6ac5a205c08df65e494d3440af8e0b90a
Osvaldo <osvaldo.ramirez@upr.edu>
2017-08-31 00:43:51 -0400

    Lab0-Excersice2
    commented and completed

--- Lab0/stringblack/stringblack/main.cpp
+++ Lab0/stringblack/stringblack/main.cpp
@@ -1,5 +1,5 @@
 /* 	Author: Osvaldo A. Ramirez
- ICOM5007- Lab0 Ex. 11716
+ ICOM5007- Lab0 Ex. 11716 uva.onlinejudge.org
  */
 
 #include <iostream>
@@ -8,51 +8,41 @@
 #include <string.h>
 #include <math.h>
 
-#define NUMOFCHAR 10001
+#define NUMOFCHAR 10001				/* Contant maximum number of characters */
 
 int main(){
 	
-    int ntd = 0;                        /* Number of words to decipher */
-    char wordToDecipher[NUMOFCHAR] ={} ;	/* Array that contains word to be deciphered */
+	int ntd = 0;                        /* Number of words to decipher */
+	char wordToDecipher[NUMOFCHAR] ={} ;	/* Array that contains word to be deciphered */
 	char wordDeciphered[NUMOFCHAR] = {};	/* Array that contains word deciphered */
-    
-    printf("Enter number of words to decipher:");  //Console message
-    
+	
+	printf("Enter number of words to decipher:");  //Console message
+	
 	scanf("%d", &ntd);          //Get number of words to decipher
 	getchar();                  //clear new line from input buffer
-    
-    //REMOVE ME
-	//printf("%d",ntd);
-	
 	
 	while(ntd >0){
-		scanf("%[^\n]",wordToDecipher);
+		scanf("%[^\n]",wordToDecipher);		//Get word to decipher
 		getchar();               //clear new line from input buffer
-	
+		
 		int length = strlen(wordToDecipher);		/* Length of the given array */
-		int root = sqrt(length);
-	
-		//wordDeciphered = {};			/* Clear Deciphered array */
+		int root = sqrt(length);					/* Root of the length of the array */
+		
 		memset(wordDeciphered, '\0', sizeof(wordDeciphered));		/* Clear Deciphered array */
 		
 		
 		if(root * root == length){		/* Check if its a perfect square */
-			//TODO Decipher
-			//puts("A lo loco");
-			int current = 0;
+			int current = 0;			/* Current index of the wordDeciphered characters */
 			for(int i =0; i < root; i++){
-				for(int j = i; j < length; j = j + root){
+				for(int j = i; j < length; j = j + root){		//j + root to decipher word
 					wordDeciphered[current++] = wordToDecipher[j];
 				}
 			}
-			printf ("%s",wordDeciphered);
+			printf ("%s",wordDeciphered);			//Print word deciphered
 		}else{
-			puts("INVALID");
+			puts("INVALID");						//Invalid if the word length is not a perfect square
 		}
-		ntd--;
+		ntd--;				//Decrease number of words to decipher
 	}
-	
-    //printf ("%s",wordToDecipher);
-
 }
 
