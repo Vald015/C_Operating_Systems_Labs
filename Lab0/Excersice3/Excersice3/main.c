b8a880e0db2f9844cd3a49180bcd3c400eae8afb
Osvaldo <osvaldo.ramirez@upr.edu>
2017-08-31 00:49:18 -0400

    Start Ex.3

--- Lab0/Excersice3/Excersice3/main.c
+++ Lab0/Excersice3/Excersice3/main.c
@@ -1,15 +1,34 @@
-//
-//  main.c
-//  Excersice3
-//
-//  Created by Osvaldo A Ramirez on 8/31/17.
-//  Copyright © 2017 Osvaldo. All rights reserved.
-//
+/* 	Author: Osvaldo A. Ramirez
+ ICOM5007- Lab0 Ex. 11716
+ */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	printf("Hello, World!\n");
-	return 0;
+
+int main(){
+	
+	int ntd = 0;                        /* Number of words to decipher */
+	
+	printf("Enter number of words to decipher:");  //Console message
+	
+	scanf("%d", &ntd);          //Get number of words to decipher
+	getchar();                  //clear new line from input buffer
+	
+	while(ntd >0){
+		//scanf("%[^\n]",wordToDecipher);
+		//getchar();               //clear new line from input buffer
+		
+		//wordDeciphered = {};			/* Clear Deciphered array */
+		//memset(wordDeciphered, '\0', sizeof(wordDeciphered));		/* Clear Deciphered array */
+		
+		//printf ("%s",wordDeciphered);
+		
+		puts("INVALID");
+		ntd--;
+	}
+
 }
+
