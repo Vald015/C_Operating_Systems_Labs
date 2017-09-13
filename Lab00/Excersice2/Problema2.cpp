b89aef6ff9000d781268d2b787bb96247817d7b0
Osvaldo <osvaldo.ramirez@upr.edu>
2017-09-13 18:00:06 -0400

    Has error

--- Lab00/Excersice2/Problema2.cpp
+++ Lab00/Excersice2/Problema2.cpp
@@ -1,4 +1,5 @@
-/*	Author: Osvaldo A. Ramirez
+/*
+ *  Author: Osvaldo A. Ramirez
  *	ICOM5007 - Laboratory work
  *	LAb00 - Excersice 2: Where's Waldorf
  */
@@ -7,38 +8,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-#define ROWS 50
-#define COLUMNS 50
-#define K 20
+#define ROWS 50       /* Set the number of rows */
+#define COLUMNS 50    /* Set the number of columns */
+#define K 20          /* Set number of rows in the number of words to find */
+
+bool checkWords(int case[2]);  /* Check different cases */
 
 int main(){
 
-  int notc = 0;
-  int rowIndex = 0;
-  int rows = 0;
-  int columns = 0;
+  int notc = 0;       /* Number of test cases */
+  int rowIndex = 0;   /* Row index */
+  int rows = 0;       /* Number of rows entered by the user */
+  int columns = 0;    /* Number of columns entered by the user */
+  int cases[8][2] = {{-1,-1}};
+  char words[20][50] = {};
 
   /*scanf("%d",&notc);
   getchar();
   printf("%d\n",notc);
   */
 
-  scanf("%d %d", &rows, &columns);
-  getchar();
+  scanf("%d %d", &rows, &columns);    /* Get the values of rows and columns from the user */
+  getchar();            /* Clear input buffer */
   //printf("%d %d",rows, columns);
-  char game[ROWS][COLUMNS] = {};
+  char game[ROWS][COLUMNS] = {};    /* Initialize game array */
 
+  /* Fill the game grid array with the characters */
   while(rowIndex != rows){
-    scanf("%s", game[rowIndex]);
-    getchar();
+    scanf("%s\n", game[rowIndex]);    /* Enter row by row */
+    getchar();    /* Clear input buffer */
     //printf("%s", game[rowIndex]);
-    rowIndex++;
+    rowIndex++;     /* Increase row index variable */
   }
+  scanf("%s\n",words[0]);
+  getchar();
+  printf("%s",words[0]);
+  /*for(int i = 0;i<rows;i++){
+    for(int j = 0;j< columns,j++){
 
-  rowIndex = 0;
-
-  printf("%s\n",game[rowIndex]);
+    }
+  }*/
+  rowIndex = 0;       /* TEST*/
+  printf("%s\n",game[rowIndex]);    /* TEST */
 
   return 0;
 
