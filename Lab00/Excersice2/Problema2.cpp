4b9a8da3da5a55cd5a5d4178aff81ac1a405c0f6
Osvaldo <osvaldo.ramirez@upr.edu>
2017-09-15 01:25:35 -0400

    WALDORF WAS FOUND

--- Lab00/Excersice2/Problema2.cpp
+++ Lab00/Excersice2/Problema2.cpp
@@ -14,22 +14,24 @@
 #define COLUMNS 50    /* Set the number of columns */
 #define K 20          /* Set number of rows in the number of words to find */
 
-int checkWords(int a[2], char b[50], int x, int y);  /* Check different cases */
+int checkCase(int a[2], char b[50], int x, int y);  /* Check different cases */
 
 int rows = 0;       /* Number of rows entered by the user */
 int columns = 0;    /* Number of columns entered by the user */
 char game[ROWS][COLUMNS] = {};    /* Initialize game array */
-int cases[8][2] = {{-1,-1}};  /* array for the 8 different cases */
+
 char words[20][50] = {};    /* Word to search in the game grid */
+int now = 0;
 
 
 int main(){
 
   int notc = 0;       /* Number of test cases */
   int rowIndex = 0;   /* Row index */
-  int cases[8][2] = {{-1,-1}};  /* array for the 8 different cases */
+  //int cases[8][2] = {{-1,-1}};  /* array for the 8 different cases */
+  int cases[8][2] = {{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1}};  /* array for the 8 different cases */
   char words[20][50] = {};    /* Word to search in the game grid */
-  int found = 0;
+  int found = 0;      /* found variable 1 when the word is found, 0 when not */
 
   /*scanf("%d",&notc);
   getchar();
@@ -47,7 +49,7 @@ int main(){
     //printf("%s", game[rowIndex]);   //Test
     rowIndex++;     /* Increase row index variable */
   }
-
+  printf("***done!!!!\n");
   /* Get word to search */
   scanf("%50s",words[0]);
   getchar();      /* Clear input buffer */
@@ -56,38 +58,45 @@ int main(){
   /* Search for the first letter */
   for(int i = 0;i<rows;i++){
     for(int j = 0;j< columns;j++){
+      /* Found first letter */
       if(words[0][0] == game[i][j]){
         printf("found it: %d // %d\n",i,j);
-        found = checkWords(cases[0],words[0],i,j);
-        if(found){
-          j = columns;
-          i = rows;
-          printf("FOUND IT");
+        /* Check each of the cases */
+        for(int q = 0 ; q<8 ; q++){
+          found = checkCase(cases[q],words[0],i,j);
+          if(found){
+            j = columns;
+            i = rows;
+            q = 8;
+            printf("FOUND IT");
+          }
         }
       }
     }
   }
   rowIndex = 0;      /* Reset row index */
   //printf("%s\n",game[rowIndex]);    /* TEST */
-
   return 0;
 }
-
-int checkWords(int a[2], char b[50],int x, int y){
-  int k = 0;
-  for(; b[k] != '\0'; ++k);
-  printf("%d\n", k);
+/* Checks a given case of a word to be found */
+int checkCase(int a[2], char b[50],int x, int y){
+  int k = 0;    /* k is the lenght of the word to be found */
+  int ro = x;
+  int co = y;
+  for(; b[k] != '\0'; ++k);   /* count characters until null value */
+  printf("k = %d\n", k);    //TEST
+  printf("case = %d %d\n", a[0],a[1]);
+  /* Check if the word is whithin the bounds of the array with the given case */
   if(((x+a[0]*(k-1)) < 0) || ((x+a[0]*(k-1)) > rows) || ((y+a[1]*(k-1)) < 0) || ((y+a[1]*(k-1)) > columns)){
-    /*int test1 = (x+a[0]*(k));
-    printf("%d\n",test1);*/
     return 0;
   }else{
+    /* Check if all the characters of the word to be found match */
     for(int l = 0;l <= k; l++){
-      if(game[x][y] == b[l]){
-        x = x + a[0];
-        printf("%d", x);
-        y = y + a[1];
-        printf("%d",y);
+      if(game[ro][co] == b[l]){
+        ro = ro + a[0];   /* x + case */
+        printf("%d ", ro);
+        co = co + a[1];   /* y + case */
+        printf("%d\n", co);
       }else{
         return 0;
       }
